f9391ba4a7d6e17a458b5c8696206ee9a5edcf35
GeniusAmirrili <amiralisandani5@gmail.com>
2026-09-07 01:09:06 +0330

    Add project files.

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'adhd-os-v1';
+const ASSETS = ['./adhd_os.html', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
